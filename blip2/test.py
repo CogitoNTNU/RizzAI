4030139aaf375fbd38fce54289034c24a2f56675
MrJoeKr <kraus.jozef225@gmail.com>
2025-09-27 23:09:47 +0200

    style: reorder imports and format code for better readability

--- blip2/test.py
+++ blip2/test.py
@@ -1,7 +1,8 @@
-from PIL import Image
 import requests
-from transformers import Blip2Processor, Blip2Model
 import torch
+from PIL import Image
+from transformers import Blip2Model, Blip2Processor
+
 
 device = "cpu"
 
@@ -12,6 +13,8 @@ url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
 
 prompt = "Her is her profile description: ... give me an opening line"
-inputs = processor(images=image, text=prompt, return_tensors="pt").to(device, torch.float16)
+inputs = processor(images=image, text=prompt, return_tensors="pt").to(
+    device, torch.float16
+)
 
 outputs = model(**inputs)
