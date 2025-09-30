eeee723ec7bea5d9ab8c2e8744882aa7459f8e8a
Kristian Carlenius <kristian.carlenius@hotmail.com>
2025-09-30 18:16:31 +0200

    setting up the q

--- blip2/rizzler.py
+++ blip2/rizzler.py
@@ -29,7 +29,18 @@ vision_config = Blip2VisionConfig() #Image encoder Keep this
 qformer_config = Blip2QFormerConfig(hidden_size=700, 
                                     num_hidden_layers=10, 
                                     num_attention_heads=10,
-                                    ) #This we want to improve and change
+                                    intermediate_size=2998,
+                                    hidden_act="gelu_new",
+                                    hidden_dropout_prob=0.08,
+                                    attention_probs_dropout_prob=0.08,
+                                    max_position_embeddings=2048,
+                                    initializer_range=0.03,
+                                    layer_norm_eps=1e-12,
+                                    pad_token_id=0,
+                                    position_embedding_type="absolute",
+                                    cross_attention_frequency=2,
+                                    encoder_hidden_size=1337,
+                                    use_qformer_text_input=False) #This we want to improve and change
 
 text_config = OPTConfig() #Language model - Keep this
 
