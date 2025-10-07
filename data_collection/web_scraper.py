80751aa8c395684922d223aa01e0dedc2ff21614
MrJoeKr <kraus.jozef225@gmail.com>
2025-10-07 19:50:03 +0200

    feat: improve error handling and restructure scraping logic

--- data_collection/web_scraper.py
+++ data_collection/web_scraper.py
@@ -3,8 +3,7 @@ import json
 import os
 import re
 import time
-import random
-
+from collections.abc import Callable
 from pathlib import Path
 
 import requests
@@ -51,11 +50,8 @@ if not os.path.exists(profile_path):
 
 options.add_argument("-profile")
 options.add_argument(profile_path)
+
 driver = webdriver.Firefox(service=Service(), options=options)
-
-driver.maximize_window()
-
-driver.get(URL)
 
 
 def get_current_person_section() -> WebElement | None:
@@ -77,8 +73,7 @@ def get_current_person_section() -> WebElement | None:
         raise Exception("No visible person's photos section found.")
 
     except Exception as e:
-        print("Could not find the person's photos section:", e)
-        return None
+        raise Exception("Could not find the person's photos section") from e
 
 
 def get_photo_url_from_section(section: WebElement, photo_index: int) -> str | None:
@@ -142,8 +137,7 @@ def get_all_them_photos() -> list[str | None]:
         return photo_urls
 
     except Exception as e:
-        print("Could not find photo elements:", e)
-        return []
+        raise Exception("Could not find photo elements") from e
 
 
 def open_more_details() -> None:
@@ -158,13 +152,13 @@ def close_more_details() -> None:
     actions.send_keys(Keys.ARROW_DOWN).perform()
 
 
-def give_like() -> None:
+def _give_like() -> None:
     """Like by pressing the right arrow key."""
     actions = webdriver.ActionChains(driver)
     actions.send_keys(Keys.ARROW_RIGHT).perform()
 
 
-def give_nonono() -> None:
+def _give_nonono() -> None:
     """Dislike by pressing the left arrow key."""
     actions = webdriver.ActionChains(driver)
     actions.send_keys(Keys.ARROW_LEFT).perform()
@@ -206,9 +200,8 @@ def get_about_me_text() -> str | None:
         )
         return about_me_div_sibling.text
 
-    except Exception:
-        print("Could not find 'About Me' section.")
-        return None
+    except Exception as e:
+        raise Exception("Could not find 'About Me' section") from e
 
 
 def get_sibling(element: WebElement) -> WebElement | None:
@@ -240,8 +233,7 @@ def get_essentials() -> list[str]:
         return essentials
 
     except Exception as e:
-        print("Could not find essential details:", e)
-        return []
+        raise Exception("Could not find essentials section") from e
 
 
 def get_basics() -> dict[str, str]:
@@ -266,8 +258,7 @@ def get_basics() -> dict[str, str]:
         return {basics[i]: basics[i + 1] for i in range(0, len(basics) - 1, 2)}
 
     except Exception as e:
-        print("Could not find basic details:", e)
-        return {}
+        raise Exception("Could not find basics section") from e
 
 
 def get_lifestyle() -> dict[str, str]:
@@ -292,8 +283,7 @@ def get_lifestyle() -> dict[str, str]:
         return {lifestyle[i]: lifestyle[i + 1] for i in range(0, len(lifestyle) - 1, 2)}
 
     except Exception as e:
-        print("Could not find lifestyle details:", e)
-        return {}
+        raise Exception("Could not find lifestyle section") from e
 
 
 def get_interests() -> list[str]:
@@ -316,8 +306,7 @@ def get_interests() -> list[str]:
         return interests
 
     except Exception as e:
-        print("Could not find interests details:", e)
-        return []
+        raise Exception("Could not find interests details") from e
 
 
 def get_anthem() -> str | None:
@@ -340,8 +329,7 @@ def get_anthem() -> str | None:
         return f"{name} by {author}"
 
     except Exception as e:
-        print("Could not find anthem details:", e)
-        return None
+        raise Exception("Could not find anthem section") from e
 
 
 def scrape_one_gyatt_or_potential_partner() -> None:
@@ -351,94 +339,139 @@ def scrape_one_gyatt_or_potential_partner() -> None:
         print("No current person section found.")
         return
 
+    name_aria = section.get_attribute("aria-label")
+    if name_aria is None:
+        print("No aria-label found for the current person section.")
+        return
+
+    name_match = re.match(r"^(.*?)'s photos$", name_aria)
+    if not name_match:
+        print("Could not extract name from aria-label.")
+        return
+
+    name = name_match.group(1)
+    print("*" * 20)
+    print(f"Scraping data for a potential partner: {name}")
+    print("*" * 20)
+
+    # Get all photo URLs
+    photo_urls = get_all_them_photos()
+
+    # Download photos
+    new_last_user_id = read_last_id() + 1
+    # Update the .last_id file with the new last user ID
+    with open(LAST_ID_PATH, "w", encoding="utf-8") as f:
+        f.write(str(new_last_user_id))
+
+    download_images(photo_urls, id=new_last_user_id)
+
+    # Open more details to access About Me and Essentials
+    open_more_details()
+    time.sleep(0.7)  # Wait for the details to load
+
+    about_me: str | None = None
     try:
-        name_aria = section.get_attribute("aria-label")
-        if name_aria is None:
-            print("No aria-label found for the current person section.")
-            return
-
-        name_match = re.match(r"^(.*?)'s photos$", name_aria)
-        if not name_match:
-            print("Could not extract name from aria-label.")
-            return
-
-        name = name_match.group(1)
-        print(f"Scraping data for a potential partner: {name}")
-
-        # Get all photo URLs
-        photo_urls = get_all_them_photos()
-
-        # Download photos
-        new_last_user_id = read_last_id() + 1
-        # Update the .last_id file with the new last user ID
-        with open(LAST_ID_PATH, "w", encoding="utf-8") as f:
-            f.write(str(new_last_user_id))
-
-        download_images(photo_urls, id=new_last_user_id)
-
-        # Open more details to access About Me and Essentials
-        open_more_details()
-        time.sleep(0.7)  # Wait for the details to load
-
         about_me = get_about_me_text()
+    except Exception:
+        print("Could not get 'About Me' text.")
+
+    essentials: list[str] = []
+    try:
         essentials = get_essentials()
+    except Exception:
+        print("Could not get 'Essentials' details.")
+
+    basics: dict[str, str] = {}
+    try:
         basics = get_basics()
+    except Exception:
+        print("Could not get 'Basics' details.")
+
+    lifestyle: dict[str, str] = {}
+    try:
         lifestyle = get_lifestyle()
+    except Exception:
+        print("Could not get 'Lifestyle' details.")
+
+    interests: list[str] = []
+    try:
         interests = get_interests()
+    except Exception:
+        print("Could not get 'Interests' details.")
+
+    anthem: str | None = None
+    try:
         anthem = get_anthem()
+    except Exception:
+        print("Could not get 'Anthem' details.")
 
-        # Close more details
-        close_more_details()
-        time.sleep(0.7)  # Small delay to allow the UI to update
+    # Close more details
+    close_more_details()
+    time.sleep(0.7)  # Small delay to allow the UI to update
 
-        # Compile data
-        data = {
-            "name": name,
-            "photos": [
-                {"index": idx, "url": url} for idx, url in enumerate(photo_urls)
-            ],
-            "about_me": about_me,
-            "essentials": essentials,
-            "basics": basics,
-            "lifestyle": lifestyle,
-            "interests": interests,
-            "anthem": anthem,
-        }
+    # Compile data
+    data = {
+        "name": name,
+        "about_me": about_me,
+        "essentials": essentials,
+        "basics": basics,
+        "lifestyle": lifestyle,
+        "interests": interests,
+        "anthem": anthem,
+    }
 
-        # Save data to JSON
-        output_path = Path("data_collection/profiles/")
-        output_path.mkdir(parents=True, exist_ok=True)
-        json_file_path = output_path / "text_data.json"
+    # Save data to JSON
+    output_path = Path("data_collection/profiles/")
+    output_path.mkdir(parents=True, exist_ok=True)
+    json_file_path = output_path / "text_data.json"
 
-        # If the file does not exist, create it with an empty dict
-        if not json_file_path.exists():
-            with open(json_file_path, "w", encoding="utf-8") as f:
-                json.dump({}, f)
+    # If the file does not exist, create it with an empty dict
+    if not json_file_path.exists():
+        with open(json_file_path, "w", encoding="utf-8") as f:
+            json.dump({}, f)
 
-        # Write the new data into the JSON file
-        # TODO: Make it more efficient by not loading the whole file every time
-        # Such as using a database (SQLite, etc.)
-        with open(json_file_path, "r+", encoding="utf-8") as f:
-            all_data = json.load(f)
-            all_data[new_last_user_id] = data
-            f.seek(0)
-            json.dump(all_data, f, ensure_ascii=False, indent=4)
-            f.truncate()
+    # Write the new data into the JSON file
+    # TODO: Make it more efficient by not loading the whole file every time
+    # Such as using a database (SQLite, etc.)
+    with open(json_file_path, "r+", encoding="utf-8") as f:
+        all_data = json.load(f)
+        all_data[new_last_user_id] = data
+        f.seek(0)
+        json.dump(all_data, f, ensure_ascii=False, indent=4)
+        f.truncate()
 
-        print(f"Data for {name} saved to {json_file_path}")
-
-    except Exception as e:
-        print("An error occurred while scraping the potential partner:", e)
+    print(f"Data for {name} saved to {json_file_path}")
+    print("*" * 20)
 
 
 # Update the function to find photos based on the specified criteria
-def scrape_website():
-    """Main function to scrape the website."""
-    try:
-        while True:
-            input("Press Enter to scrape the next potential partner...")
+def scrape_website(
+    profiles_to_scrape: int = 10, like_strategy: Callable[[], None] = _give_nonono
+) -> None:
+    """Main function to scrape the website.
 
+    Args:
+        profiles_to_scrape (int): Number of profiles to scrape.
+        like_strategy (Callable[[], None]): Function to call for liking or disliking a profile.
+    """
+    try:
+        for _ in range(profiles_to_scrape):
             scrape_one_gyatt_or_potential_partner()
+
+            like_strategy()
+
+            # Small delay to allow the next profile to load
+            time.sleep(0.8)
+
+            # Give like or nonono
+            # give_like()
+            # give_nonono()
+
+            # # Do a coinflip for like or nonono
+            # if random.random() < 0.5:
+            #     give_like()
+            # else:
+            #     give_nonono()
 
             """TODO: Sadly, we can't like them all.
             It's limited to 50-100 likes per 12 hours.
@@ -446,16 +479,6 @@ def scrape_website():
             One option would be to uniquely store them using embeddings of the images
             and then check if we already have them, but that's a bit too much work for now.
             """
-            # # Just like them all
-
-            # Do a coinflip for like or nonono
-            if random.random() < 0.5:
-                give_like()
-            else:
-                give_nonono()
-
-            # Small delay to allow the next profile to load
-            time.sleep(0.5)
 
     except KeyboardInterrupt:
         print("Scraping interrupted by user.")
@@ -480,9 +503,54 @@ def download_images(urls: list[str | None], id: int):
             print(f"Failed to download image from {url}: {e}")
 
 
-if __name__ == "__main__":
-    # Run the scraper
-    scrape_website()
+class LikeStrategy:
+    """Class to encapsulate like strategies."""
+
+    @staticmethod
+    def like() -> None:
+        _give_like()
+
+    @staticmethod
+    def nonono() -> None:
+        _give_nonono()
+
+    @staticmethod
+    def coinflip(probability: float = 0.5) -> None:
+        import random
+
+        if random.random() < probability:
+            _give_like()
+        else:
+            _give_nonono()
+
+
+def main() -> None:
+    """Main entry point for the scraper."""
+    driver.maximize_window()
+    driver.get(URL)
+
+    input("Press Enter to start scraping...")
+
+    profiles_to_scrape = 40
+    want_more = True
+    while want_more and profiles_to_scrape > 0:
+        try:
+            scrape_website(
+                profiles_to_scrape=profiles_to_scrape, like_strategy=LikeStrategy.like
+            )
+            profiles_to_scrape -= 1
+        except Exception as e:
+            print("An error occurred during scraping:", e)
+
+        want_more = (
+            input("Do you want to scrape more profiles? (y/n): ").strip().lower() == "y"
+        )
+
+    print("Scraping completed.")
 
     # Always quit the driver
     driver.quit()
+
+
+if __name__ == "__main__":
+    main()
