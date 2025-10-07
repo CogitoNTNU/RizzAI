192bc2d7146e08749914287678abc969bd30ad1b
CraZyB1336 <chrisbraat5@gmail.com>
2025-10-07 17:59:47 +0200

    feat: add colors

--- send-job.sh
+++ send-job.sh
@@ -3,6 +3,7 @@ RED='\e[31m'
 GREEN='\e[32m'
 MAGENTA='\e[35m'
 CYAN='\e[36m'
+SALMON='\033[38;5;204;48;5;231m'
 RESET='\e[0m\033[0m'
 ITALIC='\e[3m'
 BOLD='\033[1m'
@@ -12,11 +13,11 @@ JOB_OUTPUT_DIR="/cluster/home/kristiac/rizzai/RizzAI/job_output"
 JOB_INPUT="$1"
 
 if [[ "$JOB_INPUT" == *.slurm ]]; then
-    echo "Found Job file \n"
+    echo -e "${BOLD}${SALMON}[RizzAI]${RESET} Found Job file"
 else
     echo "Job file was not found. Create a valid slurm file, you noob!"
     exit 1
 fi
 
-BATCH_ID=$(sbatch $JOB_INPUT)
-echo "$BATCH_ID"
+BATCH_ID=$(sbatch $JOB_INPUT | grepo -o '[0-9]*')
+echo "${BOLD}${SALMON}[RizzAI]${RESET} Sent out batch jon. Job ID: ${GREEN}$BATCH_ID${RESET}"
