13ed8046a6f462a2dcf25767a88825f06882c2dc
MrJoeKr <kraus.jozef225@gmail.com>
2025-10-08 23:52:43 +0200

    feat: enhance web scraping functionality and improve error handling

--- data_collection/web_scraper.py
+++ data_collection/web_scraper.py
@@ -36,6 +36,12 @@ def read_last_id() -> int:
         return -1  # Default value if the file does not exist
 
 
+def write_last_id(last_id: int) -> None:
+    """Write the last used user ID to the .last_id file."""
+    with open(LAST_ID_PATH, "w", encoding="utf-8") as f:
+        f.write(str(last_id))
+
+
 # Get the last used user ID from the .last_id file
 last_user_id: int = read_last_id()
 
@@ -54,7 +60,7 @@ options.add_argument(profile_path)
 driver = webdriver.Firefox(service=Service(), options=options)
 
 
-def get_current_person_section() -> WebElement | None:
+def get_current_person_section() -> WebElement:
     """Get section where aria-label is '(NAME)'s photos'."""
     try:
         # Find all sections and filter for the one with the correct aria-label
@@ -76,7 +82,7 @@ def get_current_person_section() -> WebElement | None:
         raise Exception("Could not find the person's photos section") from e
 
 
-def get_photo_url_from_section(section: WebElement, photo_index: int) -> str | None:
+def get_photo_url_from_section(section: WebElement, photo_index: int) -> str:
     """Get the photo URL from the given section and photo index."""
     try:
         # Find the div with the specific aria-label for the photo index
@@ -86,8 +92,7 @@ def get_photo_url_from_section(section: WebElement, photo_index: int) -> str | N
         photo_div_html = photo_div.get_attribute("outerHTML")
 
         if photo_div_html is None:
-            print(f"No outerHTML found for photo index {photo_index}.")
-            return None
+            raise Exception(f"No outerHTML found for photo index {photo_index}.")
 
         # Extract URL from style attribute
         match = re.search(r'url\("?([^"\)]+)"?\)', photo_div_html)
@@ -95,14 +100,14 @@ def get_photo_url_from_section(section: WebElement, photo_index: int) -> str | N
             url = match.group(1)
             return html.unescape(url)
         else:
-            print(f"No URL found in the style attribute for photo index {photo_index}.")
-            return None
+            raise Exception(
+                f"No URL found in the style attribute for photo index {photo_index}."
+            )
     except Exception as e:
-        print(f"Could not find photo div for index {photo_index}:", e)
-        return None
+        raise Exception(f"Could not find photo div for index {photo_index}") from e
 
 
-def get_all_them_photos() -> list[str | None]:
+def get_all_them_photos() -> list[str]:
     """Get all photo elements in the current person's section."""
     section = get_current_person_section()
     if section is None:
@@ -115,9 +120,7 @@ def get_all_them_photos() -> list[str | None]:
 
         while True:
             url = get_photo_url_from_section(section, photo_index)
-
-            if url is not None:
-                url = url.strip('"')  # Remove any enclosing quotes
+            url = url.strip('"')  # Remove any enclosing quotes
 
             photo_urls.append(url)
             photo_index += 1
@@ -164,7 +167,7 @@ def _give_nonono() -> None:
     actions.send_keys(Keys.ARROW_LEFT).perform()
 
 
-def get_more_details_section(name: str) -> WebElement | None:
+def get_more_details_section(name: str) -> WebElement:
     try:
         return driver.find_element(
             By.XPATH,
@@ -172,8 +175,7 @@ def get_more_details_section(name: str) -> WebElement | None:
         )
 
     except Exception as e:
-        print("Could not find more details section:", e)
-        return None
+        raise Exception(f"Could not find more details section: {name}") from e
 
 
 def get_about_me_text() -> str | None:
@@ -182,14 +184,7 @@ def get_about_me_text() -> str | None:
         # go_into_more_details()
 
         # Get about by finding h2 with text "About Me"
-        try:
-            about_me_section = get_more_details_section("About me")
-        except Exception:
-            return None  # No "About me" section
-
-        if about_me_section is None:
-            print("No 'About me' section found.")
-            return None
+        about_me_section = get_more_details_section("About me")
 
         # Get uncle of about_me_element, so next div
         about_me_parent = about_me_section.find_element(By.XPATH, "..")
@@ -204,13 +199,12 @@ def get_about_me_text() -> str | None:
         raise Exception("Could not find 'About Me' section") from e
 
 
-def get_sibling(element: WebElement) -> WebElement | None:
+def get_sibling(element: WebElement) -> WebElement:
     """Get the next sibling of the given element."""
     try:
         return element.find_element(By.XPATH, "following-sibling::*")
     except Exception as e:
-        print("Could not find sibling element:", e)
-        return None
+        raise Exception("Could not find sibling element") from e
 
 
 def get_essentials() -> list[str]:
@@ -218,22 +212,14 @@ def get_essentials() -> list[str]:
     try:
         essentials_div = get_more_details_section("Essentials")
 
-        if essentials_div is None:
-            print("No 'Essentials' section found.")
-            return []
-
         essentials_div_parent = essentials_div.find_element(By.XPATH, "..")
         essentials_div_sibling = get_sibling(essentials_div_parent)
-
-        if essentials_div_sibling is None:
-            print("No sibling found for 'Essentials' div.")
-            return []
 
         essentials = essentials_div_sibling.text.split("\n")
         return essentials
 
     except Exception as e:
-        raise Exception("Could not find essentials section") from e
+        raise Exception("Could not find 'Essentials' section") from e
 
 
 def get_basics() -> dict[str, str]:
@@ -241,16 +227,8 @@ def get_basics() -> dict[str, str]:
     try:
         basics_div = get_more_details_section("Basics")
 
-        if basics_div is None:
-            print("No 'Basics' section found.")
-            return {}
-
         basics_div_parent = basics_div.find_element(By.XPATH, "..")
         basics_div_sibling = get_sibling(basics_div_parent)
-
-        if basics_div_sibling is None:
-            print("No sibling found for 'Basics' div.")
-            return {}
 
         basics = basics_div_sibling.text.split("\n")
 
@@ -258,7 +236,7 @@ def get_basics() -> dict[str, str]:
         return {basics[i]: basics[i + 1] for i in range(0, len(basics) - 1, 2)}
 
     except Exception as e:
-        raise Exception("Could not find basics section") from e
+        raise Exception("Could not find Basics section") from e
 
 
 def get_lifestyle() -> dict[str, str]:
@@ -273,17 +251,13 @@ def get_lifestyle() -> dict[str, str]:
         lifestyle_div_parent = lifestyle_div.find_element(By.XPATH, "..")
         lifestyle_div_sibling = get_sibling(lifestyle_div_parent)
 
-        if lifestyle_div_sibling is None:
-            print("No sibling found for 'Lifestyle' div.")
-            return {}
-
         lifestyle = lifestyle_div_sibling.text.split("\n")
 
         # Pair keys and values from the lifestyle list
         return {lifestyle[i]: lifestyle[i + 1] for i in range(0, len(lifestyle) - 1, 2)}
 
     except Exception as e:
-        raise Exception("Could not find lifestyle section") from e
+        raise Exception("Could not find 'Lifestyle' section") from e
 
 
 def get_interests() -> list[str]:
@@ -291,22 +265,14 @@ def get_interests() -> list[str]:
     try:
         interests_div = get_more_details_section("Interests")
 
-        if interests_div is None:
-            print("No 'Interests' section found.")
-            return []
-
         interests_div_parent = interests_div.find_element(By.XPATH, "..")
         interests_div_sibling = get_sibling(interests_div_parent)
-
-        if interests_div_sibling is None:
-            print("No sibling found for 'Interests' div.")
-            return []
 
         interests = interests_div_sibling.text.split("\n")
         return interests
 
     except Exception as e:
-        raise Exception("Could not find interests details") from e
+        raise Exception("Could not find 'Interests' section") from e
 
 
 def get_anthem() -> str | None:
@@ -314,19 +280,12 @@ def get_anthem() -> str | None:
     try:
         anthem_div = get_more_details_section("My anthem")
 
-        if anthem_div is None:
-            print("No 'Anthem' section found.")
-            return None
-
         anthem_div_parent = anthem_div.find_element(By.XPATH, "..")
         anthem_div_sibling = get_sibling(anthem_div_parent)
 
-        if anthem_div_sibling is None:
-            print("No sibling found for 'Anthem' div.")
-            return None
-
-        name, author = anthem_div_sibling.text.split("\n")
-        return f"{name} by {author}"
+        # name, author = anthem_div_sibling.text.split("\n")
+        # return f"{name} by {author}"
+        return ", ".join(anthem_div_sibling.text.split("\n"))
 
     except Exception as e:
         raise Exception("Could not find anthem section") from e
@@ -335,35 +294,28 @@ def get_anthem() -> str | None:
 def scrape_one_gyatt_or_potential_partner() -> None:
     """Scrape data for one potential partner."""
     section = get_current_person_section()
-    if section is None:
-        print("No current person section found.")
-        return
 
     name_aria = section.get_attribute("aria-label")
     if name_aria is None:
-        print("No aria-label found for the current person section.")
-        return
+        raise Exception("No aria-label found for the current person section.")
 
     name_match = re.match(r"^(.*?)'s photos$", name_aria)
     if not name_match:
-        print("Could not extract name from aria-label.")
-        return
+        raise Exception("Could not extract name from aria-label.")
 
     name = name_match.group(1)
     print("*" * 20)
     print(f"Scraping data for a potential partner: {name}")
-    print("*" * 20)
+    print("-" * 20)
 
     # Get all photo URLs
     photo_urls = get_all_them_photos()
 
     # Download photos
     new_last_user_id = read_last_id() + 1
-    # Update the .last_id file with the new last user ID
-    with open(LAST_ID_PATH, "w", encoding="utf-8") as f:
-        f.write(str(new_last_user_id))
+    write_last_id(new_last_user_id)
 
-    download_images(photo_urls, id=new_last_user_id)
+    download_images(photo_urls, new_last_user_id)
 
     # Open more details to access About Me and Essentials
     open_more_details()
@@ -444,49 +396,37 @@ def scrape_one_gyatt_or_potential_partner() -> None:
     print("*" * 20)
 
 
+class IntRef:
+    """A simple wrapper for an integer to allow mutation within nested functions."""
+
+    def __init__(self, value: int = 0) -> None:
+        self.value = value
+
+
 # Update the function to find photos based on the specified criteria
 def scrape_website(
-    profiles_to_scrape: int = 10, like_strategy: Callable[[], None] = _give_nonono
+    profiles_to_scrape: IntRef | None = None,
+    like_strategy: Callable[[], None] = _give_nonono,
 ) -> None:
     """Main function to scrape the website.
 
     Args:
-        profiles_to_scrape (int): Number of profiles to scrape.
+        profiles_to_scrape (IntWrapper | None): Number of profiles to scrape. If None, scrape indefinitely.
         like_strategy (Callable[[], None]): Function to call for liking or disliking a profile.
     """
-    try:
-        for _ in range(profiles_to_scrape):
-            scrape_one_gyatt_or_potential_partner()
+    while profiles_to_scrape is None or profiles_to_scrape.value > 0:
+        scrape_one_gyatt_or_potential_partner()
 
-            like_strategy()
+        like_strategy()
 
-            # Small delay to allow the next profile to load
-            time.sleep(0.8)
+        # Small delay to allow the next profile to load
+        time.sleep(0.8)
 
-            # Give like or nonono
-            # give_like()
-            # give_nonono()
-
-            # # Do a coinflip for like or nonono
-            # if random.random() < 0.5:
-            #     give_like()
-            # else:
-            #     give_nonono()
-
-            """TODO: Sadly, we can't like them all.
-            It's limited to 50-100 likes per 12 hours.
-            But with click nope all the time, we can get duplicates.
-            One option would be to uniquely store them using embeddings of the images
-            and then check if we already have them, but that's a bit too much work for now.
-            """
-
-    except KeyboardInterrupt:
-        print("Scraping interrupted by user.")
-    except Exception as e:
-        print("An error occurred during scraping:", e)
+        if profiles_to_scrape is not None:
+            profiles_to_scrape.value -= 1
 
 
-def download_images(urls: list[str | None], id: int):
+def download_images(urls: list[str], id: int):
     """Download images from the given URLs and save them locally."""
     save_path = Path(f"data_collection/profiles/images/{id}")
     save_path.mkdir(parents=True, exist_ok=True)
@@ -508,6 +448,13 @@ class LikeStrategy:
 
     @staticmethod
     def like() -> None:
+        """NOTE: Sadly, we can't like them all.
+
+        It's limited to 50-100 likes per 12 hours.
+        But with click nope all the time, we can get duplicates.
+        One option would be to uniquely store them using embeddings of the images
+        and then check if we already have them, but that's a bit too much work for now.
+        """
         _give_like()
 
     @staticmethod
@@ -531,20 +478,34 @@ def main() -> None:
 
     input("Press Enter to start scraping...")
 
-    profiles_to_scrape = 40
+    profiles_to_scrape = IntRef(40)
+
+    total_profiles = profiles_to_scrape.value
     want_more = True
-    while want_more and profiles_to_scrape > 0:
+    while want_more:
         try:
             scrape_website(
                 profiles_to_scrape=profiles_to_scrape, like_strategy=LikeStrategy.like
             )
-            profiles_to_scrape -= 1
+        except KeyboardInterrupt:
+            print("Scraping interrupted by user.")
         except Exception as e:
-            print("An error occurred during scraping:", e)
+            print(f"An error occurred: {e}")
+
+        profiles_scraped = total_profiles - profiles_to_scrape.value
+        print(f"Scraped {profiles_scraped}/{total_profiles} profiles.")
 
         want_more = (
             input("Do you want to scrape more profiles? (y/n): ").strip().lower() == "y"
         )
+
+        if want_more and profiles_to_scrape.value == 0:
+            try:
+                additional = int(input("How many more profiles to scrape? ").strip())
+                profiles_to_scrape.value += additional
+                total_profiles += additional
+            except ValueError:
+                print("Invalid number.")
 
     print("Scraping completed.")
 
