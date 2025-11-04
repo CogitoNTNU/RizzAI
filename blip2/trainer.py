bfcd3041208a679a927fb762f682ee84f7dd1a56
Kristian Carlenius <kristian.carlenius@hotmail.com>
2025-11-04 18:03:09 +0100

    fuck config

--- blip2/trainer.py
+++ blip2/trainer.py
@@ -14,38 +14,37 @@ from transformers import (
     TrainingArguments,
 )
 
+from transformers import (
+    Blip2VisionConfig,
+    Blip2QFormerConfig,
+    OPTConfig,
+    Blip2Config,
+    Blip2ForConditionalGeneration,
+)
 
-# Config for the model used --------->
+# Initializing a Blip2Config with Salesforce/blip2-opt-2.7b style configuration
+configuration = Blip2Config()
 
-qformer_config = Blip2QFormerConfig()
+# Initializing a Blip2ForConditionalGeneration (with random weights) from the Salesforce/blip2-opt-2.7b style configuration
+model = Blip2ForConditionalGeneration(configuration)
 
-"""hidden_size=700, 
-                                    num_hidden_layers=10, 
-                                    num_attention_heads=10,
-                                    intermediate_size=2998,
-                                    hidden_act="gelu_new",
-                                    hidden_dropout_prob=0.08,
-                                    attention_probs_dropout_prob=0.08,
-                                    max_position_embeddings=2048,
-                                    initializer_range=0.03,
-                                    layer_norm_eps=1e-12,
-                                    pad_token_id=0,
-                                    position_embedding_type="absolute",
-                                    cross_attention_frequency=2,
-                                    encoder_hidden_size=1337,
-                                    use_qformer_text_input=False) #This we want to improve and change """
+# Accessing the model configuration
+configuration = model.config
 
+# We can also initialize a Blip2Config from a Blip2VisionConfig, Blip2QFormerConfig and any PretrainedConfig
 
-vision_config = Blip2VisionConfig()  # Image encoder Keep this
-text_config = OPTConfig()  # Language model - Keep this
+# Initializing BLIP-2 vision, BLIP-2 Q-Former and language model configurations
+#vision_config = Blip2VisionConfig()
+#qformer_config = Blip2QFormerConfig()
+#text_config = OPTConfig()
 
-configuration = Blip2Config.from_text_vision_configs(vision_config, text_config)  # Blip2Config()
+#config = Blip2Config.from_text_vision_configs(vision_config, qformer_config, text_config)
+
 # <------------------------------
 
 
 # From model we generate/load the model we are going to train ----------->
-model = Blip2ForConditionalGeneration(configuration)
-
+#model = Blip2ForConditionalGeneration(configuration)
 processor = Blip2Processor.from_pretrained("Salesforce/blip2-flan-t5-xl")
 
 # <----------------
